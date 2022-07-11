f42f536a0cdf0986d1268b06149de0e0961f5d65
Mike Ellis <michael.ellis@vertexinc.com>
2022-07-11 10:31:27 -0400

    Reformats eslint Upgrades eslint-config-react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,41 +9,30 @@ module.exports = {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    ecmaVersion: 2018,
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint", "import"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+  ],
+  ignorePatterns: ["**/*.md"],
+  settings: {
+    react: {
+      version: "detect",
     },
-    parser: '@typescript-eslint/parser',
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        project: "./tsconfig.json",
-    },
-    "plugins": [
-        "react", '@typescript-eslint', 'import'
-    ],
-    extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-        'plugin:react/recommended'
-    ],
-    ignorePatterns: ["**/*.md"],
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    rules: {
-        "@typescript-eslint/no-explicit-any": "error", // Prefer 'unknown' instead
-    },
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error", // Prefer 'unknown' instead
+  },
 };
