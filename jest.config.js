76cf3ad0a88ab964e9e6b7af73aa374cce6be82f
Mike Ellis <michael.ellis@vertexinc.com>
2022-07-13 11:45:15 -0400

    Updates jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
-  moduleFileExtensions: ["js", "jsx"],
+  preset: "ts-jest",
   moduleDirectories: ["node_modules"],
   moduleNameMapper: {
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-    "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
+  testPathIgnorePatterns: ["/node_modules/"],
+  testMatch: [
+    "<rootDir>/src/__tests__/**/!(*.d).ts",
+    "<rootDir>/src/__tests__/**/*.test.tsx",
+    "<rootDir>/src/__tests__/**/*.test.ts",
+  ],
 };
