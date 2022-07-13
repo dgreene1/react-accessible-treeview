4ab3376180fc42a4b1aa0133cfcb7430ebd1b114
Mike Ellis <michael.ellis@vertexinc.com>
2022-07-13 14:54:30 -0400

    Updates imports

--- src/__tests__/MultiSelectDirectoryTree.test.tsx
+++ src/__tests__/MultiSelectDirectoryTree.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import { fireEvent, render } from "@testing-library/react";
-import React from "react";
+import * as React from "react";
 import TreeView, { flattenTree } from "..";
 import { ITreeViewProps, ITreeViewState, TreeViewAction } from "../TreeView";
 
