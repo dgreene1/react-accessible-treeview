e070016e9dd16e9b5f539a9add3a186bf8af82b5
Johnson, Douglas <Douglas.Johnson@verint.com>
2023-05-31 10:02:37 +0100

    Update the onNodeSelect tests to use userEvent instead of fireEvent

--- src/__tests__/TreeViewOnNodeSelect.test.tsx
+++ src/__tests__/TreeViewOnNodeSelect.test.tsx
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import TreeView from "../TreeView";
-import { fireEvent, render } from "@testing-library/react";
-import { flattenTree } from "../TreeView/utils";
-import { NodeId } from "../TreeView/types";
+import TreeView, {ITreeViewOnNodeSelectProps, ITreeViewProps} from "../TreeView";
+import {render} from "@testing-library/react";
+import {flattenTree} from "../TreeView/utils";
+import {INodeRendererProps} from "../TreeView/types";
+import userEvent from "@testing-library/user-event";
 
 const folder = {
   name: "",
@@ -11,26 +12,26 @@ const folder = {
     {
       name: "Fruits",
       children: [
-        { name: "Avocados" },
-        { name: "Bananas" },
-        { name: "Berries" },
-        { name: "Oranges" },
-        { name: "Pears" },
+        {name: "Avocados"},
+        {name: "Bananas"},
+        {name: "Berries"},
+        {name: "Oranges"},
+        {name: "Pears"},
       ],
     },
     {
       name: "Drinks",
       children: [
-        { name: "Apple Juice" },
-        { name: "Chocolate" },
-        { name: "Coffee" },
+        {name: "Apple Juice"},
+        {name: "Chocolate"},
+        {name: "Coffee"},
         {
           name: "Tea",
           children: [
-            { name: "Black Tea" },
-            { name: "Green Tea" },
-            { name: "Red Tea" },
-            { name: "Matcha" },
+            {name: "Black Tea"},
+            {name: "Green Tea"},
+            {name: "Red Tea"},
+            {name: "Matcha"},
           ],
         },
       ],
@@ -38,170 +39,70 @@ const folder = {
     {
       name: "Vegetables",
       children: [
-        { name: "Beets" },
-        { name: "Carrots" },
-        { name: "Celery" },
-        { name: "Lettuce" },
-        { name: "Onions" },
+        {name: "Beets"},
+        {name: "Carrots"},
+        {name: "Celery"},
+        {name: "Lettuce"},
+        {name: "Onions"},
       ],
     },
   ],
 };
 
-const data = flattenTree(folder);
-
-interface TreeViewOnNodeSelectProps {
-  selectedIds?: NodeId[];
-}
-
-function TreeViewOnNodeSelect(props: TreeViewOnNodeSelectProps) {
-  const { selectedIds } = props;
+function TreeViewOnNodeSelect({data, nodeRenderer, selectedIds, onNodeSelect}: ITreeViewProps) {
   return (
     <div>
       <TreeView
         data={data}
         aria-label="onNodeSelect"
         selectedIds={selectedIds}
-        onNodeSelect={(props) => console.log(props)}
+        onNodeSelect={onNodeSelect}
         multiSelect
         defaultExpandedIds={[1, 7, 11, 16]}
         propagateSelect
         propagateSelectUpwards
         togglableSelect
         nodeAction="check"
-        nodeRenderer={({
-          element,
-          getNodeProps,
-          handleSelect,
-          handleExpand,
-        }) => {
-          return (
-            <div {...getNodeProps({ onClick: handleExpand })}>
-              <div
-                className="checkbox-icon"
-                onClick={(e) => {
-                  handleSelect(e);
-                  e.stopPropagation();
-                }}
-              />
-              <span className="element">
-                {element.name}-{element.id}
-              </span>
-            </div>
-          );
-        }}
+        nodeRenderer={nodeRenderer}
       />
     </div>
   );
 }
 
-jest.spyOn(global.console, "log");
-
-test("onNodeSelect should be called when node manually seelcted/deselected", () => {
-  const { queryAllByRole } = render(<TreeViewOnNodeSelect />);
-  const nodes = queryAllByRole("treeitem");
-
-  const expectedSelected = {
-    element: { children: [], id: 5, name: "Oranges", parent: 1 },
-    isBranch: false,
-    isSelected: true,
-    treeState: {
-      disabledIds: new Set(),
-      controlledIds: new Set(),
-      expandedIds: new Set([1, 7, 11, 16]),
-      halfSelectedIds: new Set(),
-      isFocused: true,
-      lastAction: "SELECT_MANY",
-      lastInteractedWith: 5,
-      lastManuallyToggled: 5,
-      lastUserSelect: 5,
-      selectedIds: new Set([5]),
-      tabbableId: 5,
-    },
-  };
-  const expectedDeselected = {
-    element: { children: [], id: 5, name: "Oranges", parent: 1 },
-    isBranch: false,
-    isSelected: false,
-    treeState: {
-      disabledIds: new Set(),
-      controlledIds: new Set(),
-      expandedIds: new Set([1, 7, 11, 16]),
-      halfSelectedIds: new Set([1]),
-      isFocused: true,
-      lastAction: "SELECT_MANY",
-      lastInteractedWith: 5,
-      lastManuallyToggled: 5,
-      lastUserSelect: 5,
-      selectedIds: new Set(),
-      tabbableId: 5,
-    },
-  };
-
-  if (document.activeElement == null)
-    throw new Error(
-      `Expected to find an active element on the document (after focusing the second element with role["treeitem"]), but did not.`
+describe("onNodeSelect", () => {
+  const data = flattenTree(folder);
+  const nodeRenderer = ({
+    element,
+    getNodeProps,
+    handleSelect,
+    handleExpand,
+  }: INodeRendererProps) => {
+    return (
+      <div {...getNodeProps({onClick: handleExpand})}>
+        <div
+          className="checkbox-icon"
+          onClick={(e) => {
+            handleSelect(e);
+            e.stopPropagation();
+          }}
+        />
+        <span className="element">
+          {element.name}-{element.id}
+        </span>
+      </div>
     );
-
-  fireEvent.click(nodes[4].getElementsByClassName("checkbox-icon")[0]);
-
-  expect(console.log).toBeCalledWith(expectedSelected);
-  expect(nodes[4]).toHaveAttribute("aria-checked", "true");
-
-  fireEvent.click(nodes[4].getElementsByClassName("checkbox-icon")[0]);
-
-  expect(console.log).toBeCalledWith(expectedDeselected);
-  expect(nodes[4]).toHaveAttribute("aria-checked", "false");
-});
-
-test("onNodeSelect should be called only for interacted node", () => {
-  const { queryAllByRole } = render(<TreeViewOnNodeSelect />);
-  const nodes = queryAllByRole("treeitem");
-
-  const expected = {
-    element: { id: 1, name: "Fruits", children: [2, 3, 4, 5, 6], parent: 0 },
-    isSelected: true,
-    isBranch: true,
-    treeState: {
-      disabledIds: new Set(),
-      controlledIds: new Set(),
-      expandedIds: new Set([1, 7, 11, 16]),
-      halfSelectedIds: new Set(),
-      isFocused: true,
-      lastAction: "SELECT_MANY",
-      lastInteractedWith: 1,
-      lastManuallyToggled: 1,
-      lastUserSelect: 1,
-      selectedIds: new Set([1, 2, 3, 4, 5, 6]),
-      tabbableId: 1,
-    },
   };
+  let onNodeSelect: ((props: ITreeViewOnNodeSelectProps) => void);
+  beforeEach(() => {
+    onNodeSelect = jest.fn();
+  });
+  test("should be called when node manually selcted/deselected", async () => {
+    const {queryAllByRole} = render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer}
+                                                          onNodeSelect={onNodeSelect}/>);
+    const nodes = queryAllByRole("treeitem");
 
-  if (document.activeElement == null)
-    throw new Error(
-      `Expected to find an active element on the document (after focusing the second element with role["treeitem"]), but did not.`
-    );
-
-  fireEvent.click(nodes[0].getElementsByClassName("checkbox-icon")[0]);
-
-  expect(console.log).toBeCalledWith(expected);
-  expect(nodes[0]).toHaveAttribute("aria-checked", "true");
-  expect(nodes[1]).toHaveAttribute("aria-checked", "true");
-  expect(nodes[2]).toHaveAttribute("aria-checked", "true");
-  expect(nodes[3]).toHaveAttribute("aria-checked", "true");
-  expect(nodes[4]).toHaveAttribute("aria-checked", "true");
-  expect(nodes[5]).toHaveAttribute("aria-checked", "true");
-});
-
-test("onNodeSelect should not be called when controlled selection", () => {
-  render(<TreeViewOnNodeSelect selectedIds={[19]} />);
-  expect(console.log).not.toBeCalled();
-});
-
-describe("onNodeSelect should be called when node selected/deselected via keyboard", () => {
-  test("Enter", () => {
-    const expected = {
-      element: { children: [], id: 2, name: "Avocados", parent: 1 },
+    const expectedSelected = {
+      element: {children: [], id: 5, name: "Oranges", parent: 1},
       isBranch: false,
       isSelected: true,
       treeState: {
@@ -211,65 +112,50 @@ describe("onNodeSelect should be called when node selected/deselected via keyboa
         halfSelectedIds: new Set(),
         isFocused: true,
         lastAction: "SELECT_MANY",
-        lastInteractedWith: 2,
-        lastManuallyToggled: 2,
-        lastUserSelect: 2,
-        selectedIds: new Set([2]),
-        tabbableId: 2,
+        lastInteractedWith: 5,
+        lastManuallyToggled: 5,
+        lastUserSelect: 5,
+        selectedIds: new Set([5]),
+        tabbableId: 5,
       },
     };
-
-    const { queryAllByRole } = render(<TreeViewOnNodeSelect />);
-    const nodes = queryAllByRole("treeitem");
-
-    if (document.activeElement == null)
-      throw new Error(
-        `Expected to find an active element on the document (after focusing the second element with role["treeitem"]), but did not.`
-      );
-    nodes[0].focus(); //Fruits
-    fireEvent.keyDown(nodes[0], { key: "ArrowDown" }); //Avocados
-    fireEvent.keyDown(document.activeElement, { key: "Enter" });
-
-    expect(console.log).toBeCalledWith(expected);
-    expect(nodes[1]).toHaveAttribute("aria-checked", "true");
-  });
-  test("Space", () => {
-    const expected = {
-      element: { children: [], id: 2, name: "Avocados", parent: 1 },
+    const expectedDeselected = {
+      element: {children: [], id: 5, name: "Oranges", parent: 1},
       isBranch: false,
-      isSelected: true,
+      isSelected: false,
       treeState: {
         disabledIds: new Set(),
         controlledIds: new Set(),
         expandedIds: new Set([1, 7, 11, 16]),
-        halfSelectedIds: new Set(),
+        halfSelectedIds: new Set([1]),
         isFocused: true,
         lastAction: "SELECT_MANY",
-        lastInteractedWith: 2,
-        lastManuallyToggled: 2,
-        lastUserSelect: 2,
-        selectedIds: new Set([2]),
-        tabbableId: 2,
+        lastInteractedWith: 5,
+        lastManuallyToggled: 5,
+        lastUserSelect: 5,
+        selectedIds: new Set(),
+        tabbableId: 5,
       },
     };
 
-    const { queryAllByRole } = render(<TreeViewOnNodeSelect />);
+    await userEvent.click(nodes[4].getElementsByClassName("checkbox-icon")[0]);
+
+    expect(onNodeSelect).toBeCalledWith(expectedSelected);
+    expect(nodes[4]).toHaveAttribute("aria-checked", "true");
+
+    await userEvent.click(nodes[4].getElementsByClassName("checkbox-icon")[0]);
+
+    expect(onNodeSelect).toBeCalledWith(expectedDeselected);
+    expect(nodes[4]).toHaveAttribute("aria-checked", "false");
+  });
+
+  test("should be called only for interacted node", async () => {
+    const {queryAllByRole} = render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer}
+                                                          onNodeSelect={onNodeSelect}/>);
     const nodes = queryAllByRole("treeitem");
 
-    if (document.activeElement == null)
-      throw new Error(
-        `Expected to find an active element on the document (after focusing the second element with role["treeitem"]), but did not.`
-      );
-    nodes[0].focus(); //Fruits
-    fireEvent.keyDown(nodes[0], { key: "ArrowDown" }); //Avocados
-    fireEvent.keyDown(document.activeElement, { key: " " });
-
-    expect(console.log).toBeCalledWith(expected);
-    expect(nodes[1]).toHaveAttribute("aria-checked", "true");
-  });
-  test("Shift+ArrowUp", () => {
-    const expected = {
-      element: { id: 1, name: "Fruits", children: [2, 3, 4, 5, 6], parent: 0 },
+    const expected= {
+      element: {id: 1, name: "Fruits", children: [2, 3, 4, 5, 6], parent: 0},
       isSelected: true,
       isBranch: true,
       treeState: {
@@ -278,7 +164,7 @@ describe("onNodeSelect should be called when node selected/deselected via keyboa
         expandedIds: new Set([1, 7, 11, 16]),
         halfSelectedIds: new Set(),
         isFocused: true,
-        lastAction: "FOCUS",
+        lastAction: "SELECT_MANY",
         lastInteractedWith: 1,
         lastManuallyToggled: 1,
         lastUserSelect: 1,
@@ -287,54 +173,146 @@ describe("onNodeSelect should be called when node selected/deselected via keyboa
       },
     };
 
-    const { queryAllByRole } = render(<TreeViewOnNodeSelect />);
-    const nodes = queryAllByRole("treeitem");
+    await userEvent.click(nodes[0].getElementsByClassName("checkbox-icon")[0]);
 
-    if (document.activeElement == null)
-      throw new Error(
-        `Expected to find an active element on the document (after focusing the second element with role["treeitem"]), but did not.`
-      );
-    nodes[0].focus(); //Fruits
-    fireEvent.keyDown(nodes[0], { key: "ArrowDown" }); //Avocados
-    fireEvent.keyDown(document.activeElement, {
-      key: "ArrowUp",
-      shiftKey: true,
-    });
-
-    expect(console.log).toBeCalledWith(expected);
+    expect(onNodeSelect).toBeCalledWith(expected);
     expect(nodes[0]).toHaveAttribute("aria-checked", "true");
-  });
-  test("Shift+ArrowDown", () => {
-    const expected = {
-      element: { children: [], id: 2, name: "Avocados", parent: 1 },
-      isBranch: false,
-      isSelected: true,
-      treeState: {
-        disabledIds: new Set(),
-        controlledIds: new Set(),
-        expandedIds: new Set([1, 7, 11, 16]),
-        halfSelectedIds: new Set(),
-        isFocused: true,
-        lastAction: "FOCUS",
-        lastInteractedWith: 2,
-        lastManuallyToggled: 2,
-        lastUserSelect: 1,
-        selectedIds: new Set([2]),
-        tabbableId: 2,
-      },
-    };
-
-    const { queryAllByRole } = render(<TreeViewOnNodeSelect />);
-    const nodes = queryAllByRole("treeitem");
-
-    if (document.activeElement == null)
-      throw new Error(
-        `Expected to find an active element on the document (after focusing the second element with role["treeitem"]), but did not.`
-      );
-    nodes[0].focus(); //Fruits
-    fireEvent.keyDown(nodes[0], { key: "ArrowDown", shiftKey: true }); //Avocados
-
-    expect(console.log).toBeCalledWith(expected);
     expect(nodes[1]).toHaveAttribute("aria-checked", "true");
+    expect(nodes[2]).toHaveAttribute("aria-checked", "true");
+    expect(nodes[3]).toHaveAttribute("aria-checked", "true");
+    expect(nodes[4]).toHaveAttribute("aria-checked", "true");
+    expect(nodes[5]).toHaveAttribute("aria-checked", "true");
+  });
+
+  test("should not be called when controlled selection", () => {
+    render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer} selectedIds={[19]} onNodeSelect={onNodeSelect} />);
+
+    expect(onNodeSelect).not.toBeCalled();
+  });
+
+  describe("should be called when node selected/deselected via keyboard", () => {
+    test("Enter", async () => {
+      const expected= {
+        element: {children: [], id: 2, name: "Avocados", parent: 1},
+        isBranch: false,
+        isSelected: true,
+        treeState: {
+          disabledIds: new Set(),
+          controlledIds: new Set(),
+          expandedIds: new Set([1, 7, 11, 16]),
+          halfSelectedIds: new Set(),
+          isFocused: true,
+          lastAction: "SELECT_MANY",
+          lastInteractedWith: 2,
+          lastManuallyToggled: 2,
+          lastUserSelect: 2,
+          selectedIds: new Set([2]),
+          tabbableId: 2,
+        },
+      };
+
+      const {queryAllByRole} = render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer}
+                                                            onNodeSelect={onNodeSelect}/>);
+      const nodes = queryAllByRole("treeitem");
+
+      await userEvent.tab(); //Fruits
+      await userEvent.keyboard("[ArrowDown]"); //Avocados
+      await userEvent.keyboard("[Enter]");
+
+      expect(onNodeSelect).toBeCalledWith(expected);
+      expect(nodes[1]).toHaveAttribute("aria-checked", "true");
+    });
+    test("Space", async () => {
+      const expected= {
+        element: {children: [], id: 2, name: "Avocados", parent: 1},
+        isBranch: false,
+        isSelected: true,
+        treeState: {
+          disabledIds: new Set(),
+          controlledIds: new Set(),
+          expandedIds: new Set([1, 7, 11, 16]),
+          halfSelectedIds: new Set(),
+          isFocused: true,
+          lastAction: "SELECT_MANY",
+          lastInteractedWith: 2,
+          lastManuallyToggled: 2,
+          lastUserSelect: 2,
+          selectedIds: new Set([2]),
+          tabbableId: 2,
+        },
+      };
+
+      const {queryAllByRole} = render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer}
+                                                            onNodeSelect={onNodeSelect}/>);
+      const nodes = queryAllByRole("treeitem");
+
+      await userEvent.tab(); //Fruits
+      await userEvent.keyboard("[ArrowDown]"); //Avocados
+      await userEvent.keyboard(" ");
+
+      expect(onNodeSelect).toBeCalledWith(expected);
+      expect(nodes[1]).toHaveAttribute("aria-checked", "true");
+    });
+    test("Shift+ArrowUp", async () => {
+      const expected= {
+        element: {id: 1, name: "Fruits", children: [2, 3, 4, 5, 6], parent: 0},
+        isSelected: true,
+        isBranch: true,
+        treeState: {
+          disabledIds: new Set(),
+          controlledIds: new Set(),
+          expandedIds: new Set([1, 7, 11, 16]),
+          halfSelectedIds: new Set(),
+          isFocused: true,
+          lastAction: "FOCUS",
+          lastInteractedWith: 1,
+          lastManuallyToggled: 1,
+          lastUserSelect: 1,
+          selectedIds: new Set([1, 2, 3, 4, 5, 6]),
+          tabbableId: 1,
+        },
+      };
+
+      const {queryAllByRole} = render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer}
+                                                            onNodeSelect={onNodeSelect}/>);
+      const nodes = queryAllByRole("treeitem");
+
+      await userEvent.tab(); //Fruits
+      await userEvent.keyboard("[ArrowDown]"); //Avocados
+      await userEvent.keyboard("{Shift>}[ArrowUp]{/Shift}");
+
+      expect(onNodeSelect).toBeCalledWith(expected);
+      expect(nodes[0]).toHaveAttribute("aria-checked", "true");
+    });
+    test("Shift+ArrowDown", async () => {
+      const expected = {
+        element: {children: [], id: 2, name: "Avocados", parent: 1},
+        isBranch: false,
+        isSelected: true,
+        treeState: {
+          disabledIds: new Set(),
+          controlledIds: new Set(),
+          expandedIds: new Set([1, 7, 11, 16]),
+          halfSelectedIds: new Set(),
+          isFocused: true,
+          lastAction: "FOCUS",
+          lastInteractedWith: 2,
+          lastManuallyToggled: 2,
+          lastUserSelect: 1,
+          selectedIds: new Set([2]),
+          tabbableId: 2,
+        },
+      };
+
+      const {queryAllByRole} = render(<TreeViewOnNodeSelect data={data} nodeRenderer={nodeRenderer}
+                                                            onNodeSelect={onNodeSelect}/>);
+      const nodes = queryAllByRole("treeitem");
+
+      await userEvent.tab(); //Fruits
+      await userEvent.keyboard("{Shift>}[ArrowDown]{/Shift}"); //Avocados
+
+      expect(onNodeSelect).toBeCalledWith(expected);
+      expect(nodes[1]).toHaveAttribute("aria-checked", "true");
+    });
   });
 });
