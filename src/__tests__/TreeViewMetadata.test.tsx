bbb0ca7b7b4aaf8372c35214f94c8b133e8cfb09
Bret Little <bret.little@shopify.com>
2023-12-07 16:45:52 -0500

    Add boolean support. Fixes #164

--- src/__tests__/TreeViewMetadata.test.tsx
+++ src/__tests__/TreeViewMetadata.test.tsx
@@ -13,27 +13,27 @@ const initialTreeNode = {
       name: "Fruits",
       id: 54,
       children: [
-        { name: "Avocados", id: 98, metadata: { color: 'green' } },
-        { name: "Bananas", id: 789, metadata: { color: 'yellow' } },
+        { name: "Avocados", id: 98, metadata: { color: 'green', available: true } },
+        { name: "Bananas", id: 789, metadata: { color: 'yellow', available: false } },
       ],
     },
     {
       id: 888,
       name: "Drinks",
       children: [
-        { name: "Apple Juice", id: 990, metadata: { color: 'yellow' } },
-        { name: "Coffee", id: 9 , metadata: { color: 'brown' }},
+        { name: "Apple Juice", id: 990, metadata: { color: 'yellow', available: true } },
+        { name: "Coffee", id: 9 , metadata: { color: 'brown', available: true }},
         {
           id: 43,
           name: "Tea",
           children: [
-            { name: "Black Tea", id: 4,  metadata: { color: 'black' } },
-            { name: "Green Tea", id: 44, metadata: { color: 'green' } },
+            { name: "Black Tea", id: 4,  metadata: { color: 'black', available: false } },
+            { name: "Green Tea", id: 44, metadata: { color: 'green', available: false } },
             {
               id: 53,
               name: "Matcha",
-              metadata: { color: 'green' },
-              children: [{ name: "Matcha 1", id: 2, metadata: { color: 'green' } }],
+              metadata: { color: 'green', available: true },
+              children: [{ name: "Matcha 1", id: 2, metadata: { color: 'green', available: false } }],
             },
           ],
         },
@@ -101,7 +101,7 @@ function TreeViewMetadata(props: TreeViewDataTypeProps) {
       const thisNodesMetadata = nodeData.metadata;
       if (thisNodesMetadata !== undefined) {
         const node = nodes.find((x) => {
-          return x.innerHTML.includes(`${thisNodesMetadata.color}`)
+          return x.innerHTML.includes(`${thisNodesMetadata.color}`) && x.innerHTML.includes(`${thisNodesMetadata.available}`)
         });
         expect(node).toBeDefined;
      } else {
